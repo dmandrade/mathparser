ba52e6f37dcc42cd987a349868ec09646946ce79
Danilo Andrade <mandrade.danilo@gmail.com>
2018-04-04 18:52:22 -0300

    several fixes

--- Operators/Comparison/LessOrEqual.php
+++ Operators/Comparison/LessOrEqual.php
@@ -3,13 +3,13 @@
  *  Copyright (c) 2018 Webbing Brasil (http://www.webbingbrasil.com.br)
  *  All Rights Reserved
  *
- *  This file is part of the android project.
+ *  This file is part of the NomadLog Portal project.
  *
  *  @project NomadLog Portal
  *  @file LessOrEqual.php
  *  @author Danilo Andrade <danilo@webbingbrasil.com.br>
- *  @date 06/03/18 at 18:08
- *  @copyright  Copyright (c) 2017 Webbing Brasil (http://www.webbingbrasil.com.br)
+ *  @date 06/03/18 at 18:09
+ *  @copyright  Copyright (c) 2018 Webbing Brasil (http://www.webbingbrasil.com.br)
  */
 
 namespace App\MathParser\Operators\Comparison;
